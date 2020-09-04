3ac0a40b2fa44e51378e42f224ecf52f24cedfd7
hasyrails <hasyrails@gmail.com>
2020-09-05 02:33:16 +0900

    #9 / npm install @mdi/font インストール取り消し
    git revert する

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -6,11 +6,20 @@
 
 <script>
 import Calendar from "../javascript/components/Calendar";
+import Icon from '@mdi/font'
+import { mdiAccount } from '@mdi/js'
 
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      <Icon path={mdiAccount}
+        title="User Profile"
+        size={1}
+        horizontal
+        vertical
+        rotate={90}
+        color="red"
+        spin/>
     }
   },
   components: {
--- app/javascript/components/Calendar.vue
+++ app/javascript/components/Calendar.vue
@@ -29,7 +29,7 @@ export default {
   name: 'Calendar',
   data() {
     return {
-      currentDate: moment(),
+      currentDate: moment().format('YYYY/M'),
     };
   },
   methods: {
