a87dd078e0bc5c1d66afde7201fd92d222b804c7
hasyrails <hasyrails@gmail.com>
2020-09-07 02:57:44 +0900

    #55 / CountButtonコンポーネント作成で値を別々に変更成功

---
+++ app/javascript/components/CounterButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="countUp">{{ message }}: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      count: 0
+    }
+  },
+  props:{
+    // count: {
+    //   type: Number
+    // },
+    message: {
+      type: String
+    },
+  },
+  methods: {
+    countUp(){
+      this.count ++
+    }
+    // countUp(){
+    //   this.$emit('countUp')
+    // }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
