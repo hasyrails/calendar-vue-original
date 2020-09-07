e4ab8d352405bcf8de3bad84a44f726af1570dbc
hasyrails <hasyrails@gmail.com>
2020-09-08 06:44:49 +0900

    #54 / 入力項目のdataオブジェクトを作成

--- app/javascript/pages/Register.vue
+++ app/javascript/pages/Register.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>Register</div>  
+  <div class="row">
+		<div class="col-md-8">
+		<h1>Step:{{ stepNumber}}</h1>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Register'
+  name: 'Register',
+  data(){
+    return{
+      stepNumber: 1,
+			form: {
+				userName: null,
+        email: null,
+        password: null,
+        passwordConfirmation: null
+			}
+    }
+  }
 }
 </script>
 
