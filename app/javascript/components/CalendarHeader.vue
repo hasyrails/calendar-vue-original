dc79ef0134e644df077cb0f1bd189a6436a6ac2d
hasyrails <hasyrails@gmail.com>
2020-09-21 11:31:58 +0900

    #84 / 吹き出し実装完了（トップカレンダーのみ）

--- app/javascript/components/CalendarHeader.vue
+++ app/javascript/components/CalendarHeader.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="calendar-header">
     <div class="calendar-header-content">
-      <ChevronLeft fillColor="white"  @click="prev" :size="100"></ChevronLeft>
+      <div>
+        <ChevronLeft fillColor="white"  @click="prev" :size="100"></ChevronLeft>
+      </div>
       <div class="calendar-header-date">{{ currentDate }}</div>
-      <ChevronRight fillColor="white" @click="next" :size="100"></ChevronRight>
+      <div>
+        <ChevronRight fillColor="white" @click="next" :size="100"></ChevronRight>
+      </div>
     </div>
+    <router-link to="/ganttchart" style="text-decoration:none;">
+      <div class="link-to-ganttchart-page"
+      @mouseover="ballonDisplay" @mouseleave="ballonNonDisplay">
+          <div>
+            <Pencil :size="80"></Pencil>
+          </div>
+          <div class="link-description-ballon"
+          v-if="ballonFlag">
+            スケジュールを<br>立てましょう！
+          </div>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -12,10 +28,16 @@
 // import 'vue-material-design-icons/styles.css';
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
 import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
+import Pencil from 'vue-material-design-icons/Pencil.vue';
 
 
 export default {
   name: 'CalendarHeader',
+  data(){
+    return{
+      ballonFlag: false,
+    }
+  },
   props:{
     currentDate: {
       type: String,
@@ -25,6 +47,7 @@ export default {
   components:{
     ChevronLeft,
     ChevronRight,
+    Pencil
   },
   methods:{
     prev(){
@@ -32,7 +55,13 @@ export default {
     },
     next(){
       this.$emit('next')
-    }
+    },
+    ballonDisplay(){
+      this.ballonFlag = true
+    },
+    ballonNonDisplay(){
+      this.ballonFlag = false
+    },
   }
 }
 </script>
@@ -48,7 +77,7 @@ export default {
 .calendar-header-content {
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items:space-between;
   /* position:fixed; */
   bottom:1200px;
   margin-left:1%;
@@ -56,7 +85,34 @@ export default {
 .calendar-header-date {
   color: white;
   font-size: 75px;
-  margin-left: 5%;
-  margin-right: 5%;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+.link-to-ganttchart-page{
+  text-align: right;
+  margin-bottom: 15%;
+  margin-left: 85%;
+  display: flex;
+}
+
+.link-description-ballon {
+  position: relative;
+  margin: 1.5em 0 1.5em 30px;
+  padding: 15px 15px;
+  /* min-width: 100px; */
+  max-width: 250px;
+  color: #555;
+  font-size: 16px;
+  background: #e0edff;
+  border-radius:10px 10px 10px 10px;
+}
+.link-description-ballon::before{
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: -30px;
+  margin-top: -15px;
+  border: 15px solid transparent;
+  border-right: 15px solid #e0edff;
 }
 </style>
