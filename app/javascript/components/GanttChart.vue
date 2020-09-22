038d5bf6a707636724136551130d7e489b37e88a
hasyrails <hasyrails@gmail.com>
2020-09-22 11:55:56 +0900

    #98 / scheduleをmapStateで反映成功

--- app/javascript/components/GanttChart.vue
+++ app/javascript/components/GanttChart.vue
@@ -63,6 +63,7 @@ import draggable from 'vuedraggable'
 
 import GanttChartHeader from "../components/GanttChartHeader";
 import Schedule from "../components/Schedule"
+import { mapState } from 'vuex'
 // import ScheduleSettingModal from "../components/ScheduleSettingModal"
 
 export default {
@@ -95,38 +96,6 @@ export default {
       currentYYMMDD: moment().format('YYYY/MM/DD'),
       currentMonth: moment().month()+1,
       currentYear: moment().year(),
-      schedules: [
-        {
-          title: 'Rails',
-          start_yyyymmdd: moment('2020-09-07'),
-          start_date: moment('2020-09-07').date(),
-          end_yyyymmdd: moment('2020-09-10'),
-          end_date: moment('2020-09-010').date(),
-          color: '#FFD5EC',
-          // icon: 0,
-          commit: true
-        },
-        {
-          title: 'ポートフォリオ',
-          start_yyyymmdd: moment('2020-09-07'),
-          start_date: moment('2020-09-07').date(),
-          end_yyyymmdd: moment('2020-09-10'),
-          end_date: moment('2020-09-10').date(),
-          color: '#CBFFD3',
-          // icon: 0,
-          commit: true
-        },
-        {
-          title: 'Docker制覇',
-          start_yyyymmdd: moment('2020-09-07'),
-          start_date: moment('2020-09-07').date(),
-          end_yyyymmdd: moment('2020-09-10'),
-          end_date: moment('2020-09-10').date(),
-          color: '#CCFFFF',
-          // icon: 0,
-          commit: true　
-        },
-      ],
     };
   },
   components: {
@@ -344,6 +313,9 @@ export default {
     scheduleNum() {
       return this.calendar.scheduleNum;
     },
+    ...mapState('schedules',{
+      schedules: 'schedules'
+    })
   },
   created(){
     console.log(currentDate);
--- app/javascript/store/modules/schedules.js
+++ app/javascript/store/modules/schedules.js
@@ -1,70 +1,50 @@
+import moment from 'moment'
+
 const savedSchedules = localStorage.getItem('schedules')
 
 const schedules = {
   namespaced: true,
     state: {
-    schedules: savedSchedules ? JSON.parse(savedSchedules): [
-      {
-        title: 'Backlog',
-        cards: [
-          { body: 'English' },
-          { body: 'Mathematics' },
-        ]
-      },
-      {
-        title: 'Todo',
-        cards: [
-          { body: 'Science' }
-        ]
-      },
-      {
-        title: 'Doing',
-        cards: []
-      }
+    // schedules: savedSchedules ? JSON.parse(savedSchedules): [
+      schedules: [
+        {
+          title: 'Rails',
+          start_yyyymmdd: moment('2020-09-07'),
+          start_date: moment('2020-09-07').date(),
+          end_yyyymmdd: moment('2020-09-10'),
+          end_date: moment('2020-09-010').date(),
+          color: '#FFD5EC',
+          // icon: 0,
+          commit: true
+        },
+        {
+          title: 'ポートフォリオ',
+          start_yyyymmdd: moment('2020-09-07'),
+          start_date: moment('2020-09-07').date(),
+          end_yyyymmdd: moment('2020-09-10'),
+          end_date: moment('2020-09-10').date(),
+          color: '#CBFFD3',
+          // icon: 0,
+          commit: true
+        },
+        {
+          title: 'Docker制覇',
+          start_yyyymmdd: moment('2020-09-07'),
+          start_date: moment('2020-09-07').date(),
+          end_yyyymmdd: moment('2020-09-10'),
+          end_date: moment('2020-09-10').date(),
+          color: '#CCFFFF',
+          // icon: 0,
+          commit: true　
+        },
     ],
   },
   mutations: {
-    addlist(state, payload) {
-      state.lists.push({ title: payload.title, cards:[] })
-    },
-    removelist(state, payload) {
-      state.lists.splice(payload.listIndex, 1)
-    },
-    addCardToList(state, payload) {
-      state.lists[payload.listIndex].cards.push({ body: payload.body })
-    },
-    removeCardFromList(state, payload) {
-      state.lists[payload.listIndex].cards.splice(payload.cardIndex, 1)
-    },
-    updateList(state, payload) {
-      state.lists = payload.lists
-    }
   },
   actions: {
-    addlist(context, payload) {
-      context.commit('addlist', payload)
-    },
-    removelist(context, payload) {
-      context.commit('removelist', payload)
-    },
-    addCardToList(context, payload) {
-      context.commit('addCardToList', payload)
-    },
-    removeCardFromList(context, payload) {
-      context.commit('removeCardFromList', payload)
-    },
-    updateList(context, payload) {
-      context.commit('updateList', payload)
-    }
   },
   getters: {
-    // totalCardCount(state) {
-    //   let count = 0
-    //   state.lists.map(content => count += content.cards.length)
-    //   return count
-    // },
   },
-
 }
 
 export default schedules;
