0f5876539e36097be2ced97f8ced9a72f0987cc0
hasyrails <hasyrails@gmail.com>
2020-09-22 13:51:58 +0900

    Revert "#98 / momentの日付関連のstore移行中、エラー"
    This reverts commit c330e7c551dc0591fc565122704d45a6d89dfebc.

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -3,9 +3,6 @@ import Vuex from 'vuex'
 
 import lists from '../store/modules/lists'
 import schedules from '../store/modules/schedules'
-// import calendar from '../store/modules/calendar'
-import ganttchart from '../store/modules/ganttchart'
-import date from '../store/modules/date'
 
 Vue.use(Vuex)
 
@@ -14,9 +11,7 @@ const store =  new Vuex.Store({
   modules: {
     lists,
     schedules,
-    // calendar
-    ganttchart,
-    date
+    // devidedSchedules,
   }
 })
 
