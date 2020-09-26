554ce68e7d11db0371dfe3f5e2bea2821dfbc693
hasyrails <hasyrails@gmail.com>
2020-09-27 08:13:47 +0900

    #109 / Fakerをインストール

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'time'
+
+start_time = Time.new(2020,9,27)
+end_time = Time.new(2020,9,30)
+
+5.times do
+  Schedule.create!(
+    title: Faker::Book.title,
+    start: start_time,
+    start_year: start_time.year.to_i,
+    start_month: start_time.month.to_i,
+    start_date: start_time.day.to_i,
+    end: end_time,
+    end_year: end_time.year.to_i,
+    end_month: end_time.month.to_i,
+    end_date: end_time.day.to_i,
+  )
+end
