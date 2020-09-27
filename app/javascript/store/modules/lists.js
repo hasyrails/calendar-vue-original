2174605cd913a3e2b9c215a2ce873ae3050f2c03
hasyrails <hasyrails@gmail.com>
2020-09-27 13:27:51 +0900

    #113 / Cardsテーブルを作成

--- app/javascript/store/modules/lists.js
+++ app/javascript/store/modules/lists.js
@@ -10,7 +10,7 @@ const lists = {
       {
         title: '',
         cards: [
-          { body: '' },
+          { description: '' },
         ]
       },
     ],
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_005459) do
+ActiveRecord::Schema.define(version: 2020_09_27_042243) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
