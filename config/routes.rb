52bde980acccaa8cf2d23ea2c5e21718092dc265
hasyrails <hasyrails@gmail.com>
2020-09-27 14:48:37 +0900

    #113 / Api::CardsController作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   namespace :api, { format: "json" } do
     resources :schedules
-    resources :lists
+    resources :lists 
+    resources :cards
   end
   get '*path', to: 'home#index'
 end
