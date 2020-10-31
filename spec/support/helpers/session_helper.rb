540c43f4a43b9c5deb432bb8a00a8eaf99cb78d6
hasyrails <hasyrails@gmail.com>
2020-10-31 21:06:28 +0900

    #278 / トップページのSystemSpecを実装

---
+++ spec/support/helpers/session_helper.rb
@@ -0,0 +1,24 @@
+#/spec/support/helpers/session_helper.rb 
+module SessionHelper 
+
+  def set_request_headers(resp) 
+  { 'ACCEPT' => "application/json", 
+   'Content-Type' => "application/json", 
+   'access-token' => resp['access-token'], 
+   'token-type' => resp['token-type'], 
+   'client' => resp['client'], 
+   'expiry' => resp['expiry'], 
+   'uid' => resp['uid'] 
+  } 
+  end 
+
+  def subdomain_login(uid, password, subdomain) 
+  request_params = { 
+   'email' => uid, 
+   'password' => password 
+  } 
+  host! "#{subdomain}.lvh.me" 
+  post "/portal/auth/sign_in", params: request_params 
+  return set_request_headers(response.headers) 
+  end 
+end 
