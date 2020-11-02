b5b2fb84c54f81976f08f4c1a41f88bef1ed972c
hasyrails <hasyrails@gmail.com>
2020-11-02 18:29:32 +0900

    #311 / deviseのコールバックを実装（user）
    エラー画面(notFound)を修正
    ref #316

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,6 +1,7 @@
 class Api::ListsController < ApplicationController
   before_action :set_list, only: %i[show update destroy]
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_api_user!
 
   def index
     @lists = List.all
