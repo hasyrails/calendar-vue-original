79def6264ca3f245f19bba0fe21bfdea5eb22a8e
hasyrails <hasyrails@gmail.com>
2020-11-10 07:49:11 +0900

    deploy.rb修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :application, 'calendar-vue-original'
 set :repo_url, 'git@github.com:hasyrails/calendar-vue-original.git'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
-set :branch, 'aws_deploy_12_spocket'
+set :branch, 'master'
 
 # deploy先のディレクトリ。 
 set :deploy_to, '/var/www/calendar-vue-original'
