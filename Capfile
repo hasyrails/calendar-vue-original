54447c76a2ff2f5f16f29df57b071b9c2d6e1c25
hasyrails <hasyrails@gmail.com>
2020-11-14 16:55:51 +0900

    ActionView::Template::Error (Webpacker can't find hello_vue.js

--- Capfile
+++ Capfile
@@ -2,7 +2,7 @@ require 'capistrano/setup'
 require 'capistrano/deploy'
 require 'capistrano/rbenv' 
 require 'capistrano/bundler'
-# require 'capistrano/rails/assets' 
+require 'capistrano/rails/assets' 
 require 'capistrano/rails/migrations' 
 
 # require 'capistrano3/unicorn'
@@ -10,5 +10,6 @@ require 'capistrano/rails/migrations'
 # require 'capistrano/chruby'
 # require 'capistrano/passenger'
 
-# taskを記述したファイルを読み込むよう設定。　場所と拡張子を指定。
+# taskを記述したファイルを読み込むよう設定。場所と拡張子を指定。
+# Dir.glob('lib/capistrano/tasks/*.rb').each { |r| import r }
 Dir.glob('lib/capistrano/tasks/*.rb').each { |r| import r }
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,49 +1,22 @@
 <template>
   <div id="app">
-    <Header v-if="!showDone"></Header>
-    <HeaderOfDone v-if="showDone"></HeaderOfDone>
-    <router-view >
-    </router-view>
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Header from '../javascript/components/Header'
-import HeaderOfDone from '../javascript/components/HeaderOfDone'
-
 export default {
   data: function () {
     return {
-      // showDone: false
+      message: "Hello Vue!"
     }
-  },
-  components: {
-    Header,
-    HeaderOfDone,
-  },
-  methods:{
-    // showDoneSchedules(){
-    //   this.showDone = true
-    // }
-  },
-  computed:{
-    ...mapGetters([
-      'showDone'
-    ]),
-  },
-  beforeCreate(){
-    const existingSession = this.$cookies.get('session')
-
-    if (existingSession && existingSession.length) { // A string at this point
-      const session = JSON.parse(existingSession)
-      this.$store.commit('auth/signIn', session.user)
-      // this.$store.commit('auth', session.tokens)
-    }
-  },
+  }
 }
 </script>
 
 <style scoped>
-
+p {
+  font-size: 2em;
+  text-align: center;
+}
 </style>
